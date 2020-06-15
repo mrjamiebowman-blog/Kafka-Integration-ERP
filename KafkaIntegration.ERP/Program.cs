d59915237c8f9d25ac571abceb3c12afc603853a
Jamie Bowman <mrjamiebowman@gmail.com>
2020-06-14 20:45:36 -0400

    initial commit.

---
+++ KafkaIntegration.ERP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaIntegration.ERP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
